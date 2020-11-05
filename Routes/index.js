5f9d4490e9fd88da9e0961dd00f4f36f86461174
Joanna Pilika <joannap@joannas-mbp.myfiosgateway.com>
2020-11-05 14:41:04 -0500

    added express and environmental files

--- Routes/index.js
+++ Routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-outer.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.send('Routed Exercise Four');
 });
 
