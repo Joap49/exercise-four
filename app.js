000179ea715268c14df9f518a39cc5414f6ebf1a
Joanna Pilika <joannap@joannas-mbp.myfiosgateway.com>
2020-11-05 16:09:21 -0500

    Changed Router to router

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 const port = process.env.PORT  || 4000;
 
 //Routes Import
-const indexRoute = require('./Routes./index.js');
+const indexRoute = require('./routes./index.js');
 //Routes
-app.use('/', indexRoute);
+app.use("/", indexRoute);
 
 app.listen(port, () => console.log(`Exercise Four is running at localhost:${port}`)
 );
