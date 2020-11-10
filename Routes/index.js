14ad1de6ceb34481ef6d47d83e9b314e3734420c
Joanna Pilika <joannap@joannas-mbp.myfiosgateway.com>
2020-11-10 14:43:58 -0500

    Firebase

--- Routes/index.js
+++ Routes/index.js
@@ -1,8 +1,29 @@
+//Show al blogposts
 const express = require("express");
-
 const router = express.Router();
+//Require Firebase
+const firebase = require("firebase");
+//Initialize firebase
+const db = firebase.firestore();
+//reference a specific collection
+const blogposts = db.collection("blogposts");
 
-router.get("/", (req, res) => res.send("Exercise Four"));
+router.get("/", (req, res) => {
+    // Inside this arrow funciton, we can do anything we want as long as we return at the end
+    blogposts.get()
+    .then((querySnapshot) => {
+        //loop through quert snapshot and push into array
+        console.log("querySnapshot", querySnapshot);
+        querySnapshot.forEach((doc) => {
+            blogpostsArray.push(doc.data());
+        });
+        //return array
+        return res.send(blogpostsArray);
+    }).catch( function (e) {
+        console.warn('error:', e)
+        return res.send(error);
+    });
+});
 
 
 module.exports = router;
