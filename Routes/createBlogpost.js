6a6c4124060f8417b9c420148e0ba64ec587ac38
Joanna Pilika <joannap@joannas-mbp.myfiosgateway.com>
2020-11-17 14:25:37 -0500

    Finished work

--- Routes/createBlogpost.js
+++ Routes/createBlogpost.js
@@ -10,7 +10,7 @@ const db = firebase.firestore();
 const blogposts = db.collection("blogposts");
 
 const form = `
-<form>
+<form action="/create/submit"> 
     <input info="text" name="title" placeholder="Title of the Post" />
     <input info="text" name="text" placeholder="Text of the Post" />
     <input info="text" name="author" placeholder="Author of the Post" />
@@ -19,7 +19,25 @@ const form = `
 
 
 `;
-
+//Default route serves form
 router.get("/", (req, res) => res.send(form));
+//Route for submitting the form
+router.get("/submit", (req, res) => {
+  const queryParams = req.query;
+
+  //custom id for our posts
+  const idFromTitle = queryParams.title.replace(/\s+/g, "-").toLowerCase(); //any spaces, replace with dash
+
+  blogposts
+    .doc(idFromTitle) // alows you to create new posts or update them
+    .set(queryParams)
+    .then(function (doc) {
+      res.send("Successful Submission");
+    })
+    .catch(function (error) {
+      console.log("error", error);
+      res.send("Failed Submission");
+    });
+});
 
 module.exports = router;
